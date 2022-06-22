f375a2a21fee7fc56293bbce97e135958ef04515
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-06-22 16:54:14 -0700

    added table to display job application data
    Co-authored-by: Li Cheng 100156939+delacour124@users.noreply.github.com
    Co-authored-by: Alex Kolb akolb@clemson.edu
    Co-authored-by: Will Robson will.robson19@gmail.com
    Co-authored-by: Yang Song yangsong531@gmail.com
    Co-authored-by: Giovanni Flores-Lovo  83046950+gfloresl@users.noreply.github.com

--- client/components/MainPage.jsx
+++ client/components/MainPage.jsx
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import Container  from './FormContainer'
+import BasicTable from './Table';
 
 
 function MainPage()   {
    return (
       <div className='main-display'>
          <Container/>
+         <BasicTable/>
       </div>
    )
    

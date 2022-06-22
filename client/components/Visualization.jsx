f3e50a7d85694699ba843430d04ffed65c49bbf5
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-06-22 16:55:00 -0700

    trying to get pie chart
    Co-authored-by: Li Cheng 100156939+delacour124@users.noreply.github.com
    Co-authored-by: Alex Kolb akolb@clemson.edu
    Co-authored-by: Will Robson will.robson19@gmail.com
    Co-authored-by: Yang Song yangsong531@gmail.com
    Co-authored-by: Giovanni Flores-Lovo  83046950+gfloresl@users.noreply.github.com

--- client/components/Visualization.jsx
+++ client/components/Visualization.jsx
@@ -1,8 +1,49 @@
 
 import React from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
 
-class Visualization extends React.Component {
 
+export default function Visualization(props) {
+  const [rows, setRows] = useState([]);
+
+  const colors = ['#E38627', '#C13C37' ,  '#6A2135',   ];
+  
+  useEffect(()=>{
+    const userid = 35;
+    fetch(`/app/getStageData?userid=${userid}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log('receved data from server', JSON.stringify(data));
+      setRows(data);
+    })
+    .catch(err => console.log(err));
+  },[])
+
+
+
+// [
+//   {
+//       "stage": "in process",
+//       "count_by_stage": "1",
+//       "total_count": "4"
+//   },
+//   {
+//       "stage": "no offer",
+//       "count_by_stage": "1",
+//       "total_count": "4"
+//   },
+//   {
+//       "stage": "not start",
+//       "count_by_stage": "2",
+//       "total_count": "4"
+//   }
+// ]
+
+  <PieChart
+  data={[
+    { title: 'One', value: 10, color: '#E38627' },
+    { title: 'Two', value: 15, color: '#C13C37' },
+    { title: 'Three', value: 20, color: '#6A2135' },
+  ]}
+/>;
 }
-
-export default Visualization;

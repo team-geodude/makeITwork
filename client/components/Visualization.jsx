930ef85a2186b6d6241d9c18207167428616ca8e
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-06-22 18:07:54 -0700

    added pie chart
    Co-authored-by: Li Cheng 100156939+delacour124@users.noreply.github.com
    Co-authored-by: Alex Kolb akolb@clemson.edu
    Co-authored-by: Will Robson will.robson19@gmail.com
    Co-authored-by: Yang Song yangsong531@gmail.com
    Co-authored-by: Giovanni Flores-Lovo  83046950+gfloresl@users.noreply.github.com

--- client/components/Visualization.jsx
+++ client/components/Visualization.jsx
@@ -1,22 +1,56 @@
 
 import React from 'react';
-import { PieChart } from 'react-minimal-pie-chart';
-
+import { PieChart, Pie } from 'react-minimal-pie-chart';
+import { useState, useEffect } from "react";
 
 export default function Visualization(props) {
-
+  const [stage, setStage] = useState([]);
   
   useEffect(()=>{
-    const userid = 35;
+    const userid = 47;
     fetch(`/app/getStageData?userid=${userid}`)
     .then(res => res.json())
     .then(data => {
       console.log('receved data from server', JSON.stringify(data));
-  
+      setStage(data);
     })
     .catch(err => console.log(err));
   },[])
-
+  console.log("stage", stage);
+  const colors = ['#ff6666', '#124697', '#fca224', '#eff0f0', '#ff6222'];
+  function generateData(stage) {
+    const result = [];
+    for (let i = 0; i < stage.length; i++) {
+      let el = {};
+      el.title = stage[i].stage;
+      el.value = Number(stage[i].count_by_stage);
+      el.color = colors[i];
+      result.push(el);
+    }
+    return result;
+  }
+  const data = generateData(stage);
+  console.log('final data', data)
+  return (
+    // <PieChart width={700} height={700}>
+    //   {/* <Pie data={{stage}} dataKey="stage" outerRadius={250} fill="green" /> */}
+    // </PieChart>
+    <PieChart 
+    animate
+              animationDuration={500}
+              animationEasing="ease-out"
+              center={[50, 50]}
+              
+    style={{width:'40%', alignSelf:'center'}} label={(data) => data.dataEntry.title} labelStyle={{fontSize:'5px', alignItems:'center'}}
+      // data={[
+      //   { title: 'In Process', value: 3, color: '#fb8500' },
+      //   { title: 'No Offer', value: 1, color: '#ffb703' },
+      //   { title: 'Not Start', value: 2, color: '#023047' },
+      // ]}
+      data={data}
+    />
+  );  
+}
 
 
 // [
@@ -44,4 +78,3 @@ export default function Visualization(props) {
 //     { title: 'Three', value: 20, color: '#6A2135' }
 //   ]}}
 // />;
-}

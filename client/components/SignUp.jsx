9f49f9420ffc05ea3dbacca11716cae7a683e31c
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-06-22 18:12:40 -0700

    fixed sign up button style

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -12,7 +12,7 @@ function SignUp(props) {
           <input type='text' name='username' placeholder="Enter Username" required/><br></br>
           <label><b>Password</b></label>
           <input type='password' name='password' placeholder="Enter Password" required/><br></br>
-          <input type="submit" value="Sign Up" />
+          <input className='signupBtn' type="submit" value="Sign Up" />
           
           </div>
         </form> 

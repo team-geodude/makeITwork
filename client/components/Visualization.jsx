210ac3fd5539bbc5719c2b3d5e11b4d31fcd4cb8
Li Cheng <lilybearcheng@Lis-MacBook-Pro.home>
2022-06-22 17:00:16 -0700

    fixed code, web able to run

--- client/components/Visualization.jsx
+++ client/components/Visualization.jsx
@@ -4,9 +4,7 @@ import { PieChart } from 'react-minimal-pie-chart';
 
 
 export default function Visualization(props) {
-  const [rows, setRows] = useState([]);
 
-  const colors = ['#E38627', '#C13C37' ,  '#6A2135',   ];
   
   useEffect(()=>{
     const userid = 35;
@@ -14,7 +12,7 @@ export default function Visualization(props) {
     .then(res => res.json())
     .then(data => {
       console.log('receved data from server', JSON.stringify(data));
-      setRows(data);
+  
     })
     .catch(err => console.log(err));
   },[])
@@ -39,11 +37,11 @@ export default function Visualization(props) {
 //   }
 // ]
 
-  <PieChart
-  data={[
-    { title: 'One', value: 10, color: '#E38627' },
-    { title: 'Two', value: 15, color: '#C13C37' },
-    { title: 'Three', value: 20, color: '#6A2135' },
-  ]}
-/>;
+//   <PieChart
+//   data={{[
+//     { title: 'One', value: 10, color: '#E38627' },
+//     { title: 'Two', value: 15, color: '#C13C37' },
+//     { title: 'Three', value: 20, color: '#6A2135' }
+//   ]}}
+// />;
 }
